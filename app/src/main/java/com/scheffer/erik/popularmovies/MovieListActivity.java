a5993c8f3e6503deb2ff0a4024c4a307544b3d1e
esscheffer <erikscheffer@yahoo.com>
2017-12-03 17:45:00 -0200

    Added menu options.

--- app/src/main/java/com/scheffer/erik/popularmovies/MovieListActivity.java
+++ app/src/main/java/com/scheffer/erik/popularmovies/MovieListActivity.java
@@ -1,7 +1,10 @@
 package com.scheffer.erik.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.top_rated:
+                Toast.makeText(this, "Top rated clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.most_popular:
+                Toast.makeText(this, "Most popular clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
