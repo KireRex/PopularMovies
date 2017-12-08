0590a2d9e654fd9b692ec16158f47693e17fac6f
esscheffer <erikscheffer@yahoo.com>
2017-12-08 19:38:43 -0200

    Extracted movie API key to gradle.properties

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-The Movie Database API key must be added to the MOVIES_DATABASE_API_KEY variable on class ApiConstants before build
+The Movie Database API key must be added to the gradle.properties file like:
+API_KEY="<your api ky>"
--- app/src/main/java/com/scheffer/erik/popularmovies/MovieDatabaseApi/ApiConstants.java
+++ app/src/main/java/com/scheffer/erik/popularmovies/MovieDatabaseApi/ApiConstants.java
@@ -1,8 +1,9 @@
 package com.scheffer.erik.popularmovies.MovieDatabaseApi;
 
+import com.scheffer.erik.popularmovies.BuildConfig;
+
 public class ApiConstants {
-    // TODO: remove key before commit, add key after checkout
-    static final String MOVIES_DATABASE_API_KEY = "";
+    static final String MOVIES_DATABASE_API_KEY = BuildConfig.MOVIE_API_KEY;
     static final String MOVIES_DATABASE_BASE_URL = "https://api.themoviedb.org/3/movie/";
     public static final String MOVIES_DATABASE_BASE_POSTER_URL = "http://image.tmdb.org/t/p/";
 }
