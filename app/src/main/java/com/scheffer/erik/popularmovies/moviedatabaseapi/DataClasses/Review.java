8934c9ddbdea934e05d225a5aedb3a579800e4ab
esscheffer <erikscheffer@yahoo.com>
2017-12-17 20:24:09 -0200

    Saved movie details activity instance

--- app/src/main/java/com/scheffer/erik/popularmovies/moviedatabaseapi/DataClasses/Review.java
+++ app/src/main/java/com/scheffer/erik/popularmovies/moviedatabaseapi/DataClasses/Review.java
@@ -1,10 +1,32 @@
 package com.scheffer.erik.popularmovies.moviedatabaseapi.DataClasses;
 
-public class Review {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review implements Parcelable {
     private String id;
     private String author;
     private String content;
     private String url;
+
+    private Review(Parcel in) {
+        id = in.readString();
+        author = in.readString();
+        content = in.readString();
+        url = in.readString();
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -46,5 +68,18 @@ public class Review {
                 ", content='" + content + '\'' +
                 ", url='" + url + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(author);
+        dest.writeString(content);
+        dest.writeString(url);
     }
 }
--- app/src/main/java/com/scheffer/erik/popularmovies/moviedatabaseapi/DataClasses/Trailer.java
+++ app/src/main/java/com/scheffer/erik/popularmovies/moviedatabaseapi/DataClasses/Trailer.java
@@ -1,10 +1,32 @@
 package com.scheffer.erik.popularmovies.moviedatabaseapi.DataClasses;
 
-public class Trailer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Trailer implements Parcelable {
     private String id;
     private String key;
     private String name;
     private String site;
+
+    private Trailer(Parcel in) {
+        id = in.readString();
+        key = in.readString();
+        name = in.readString();
+        site = in.readString();
+    }
+
+    public static final Creator<Trailer> CREATOR = new Creator<Trailer>() {
+        @Override
+        public Trailer createFromParcel(Parcel in) {
+            return new Trailer(in);
+        }
+
+        @Override
+        public Trailer[] newArray(int size) {
+            return new Trailer[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -46,5 +68,18 @@ public class Trailer {
                 ", name='" + name + '\'' +
                 ", site='" + site + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(key);
+        dest.writeString(name);
+        dest.writeString(site);
     }
 }
