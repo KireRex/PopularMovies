82a2326ece372517d545af5626ef556c6b561b58
esscheffer <erikscheffer@yahoo.com>
2018-01-20 09:59:09 -0200

    DBFlow eq extension

--- app/src/main/java/com/scheffer/erik/popularmovies/database/DatabaseFacade.kt
+++ app/src/main/java/com/scheffer/erik/popularmovies/database/DatabaseFacade.kt
@@ -1,9 +1,6 @@
 package com.scheffer.erik.popularmovies.database
 
-import com.raizlabs.android.dbflow.kotlinextensions.from
-import com.raizlabs.android.dbflow.kotlinextensions.list
-import com.raizlabs.android.dbflow.kotlinextensions.select
-import com.raizlabs.android.dbflow.kotlinextensions.where
+import com.raizlabs.android.dbflow.kotlinextensions.*
 import com.scheffer.erik.popularmovies.moviedatabaseapi.models.Movie
 import com.scheffer.erik.popularmovies.moviedatabaseapi.models.Movie_Table
 
@@ -11,4 +8,4 @@ fun getAllMovies(): MutableList<Movie>? = (select from Movie::class).list
 
 fun getMovieByExternalId(externalId: Long) =
         (select from Movie::class
-                where (Movie_Table.id.eq(externalId))).querySingle()
+                where (Movie_Table.id eq externalId)).querySingle()
